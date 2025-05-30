abbf3ce688a222604a8415cff5e7b7d639b016d1
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-05-30 09:40:44 +0530

    API_Finalproject

---
+++ CoderArmy/src/main/java/org/coderarmy/CoderArmy/CoderArmyApplication.java
@@ -0,0 +1,13 @@
+package org.coderarmy.CoderArmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderArmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderArmyApplication.class, args);
+	}
+
+}
---
+++ CoderArmy/src/test/java/org/coderarmy/CoderArmy/CoderArmyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coderarmy.CoderArmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderArmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
