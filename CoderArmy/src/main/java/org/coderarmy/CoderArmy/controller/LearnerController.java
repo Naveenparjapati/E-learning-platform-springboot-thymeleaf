5193bae17ec4e5da01f947a936d858f26eac2f20
Naveen-kkkk <naveenkumarbalsamand123@gmail.com>
2025-06-08 06:51:36 +0530

    css done

--- CoderArmy/src/main/java/org/coderarmy/CoderArmy/controller/LearnerController.java
+++ CoderArmy/src/main/java/org/coderarmy/CoderArmy/controller/LearnerController.java
@@ -1,5 +1,24 @@
 package org.coderarmy.CoderArmy.controller;
 
-public class LearnerController {
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/learner")
+public class LearnerController {
+    
+	@GetMapping("/home")
+	public String loadHome(HttpSession session) {
+		if (session.getAttribute("learner") != null) {
+			return "leaner-home.html";
+		} else {
+			session.setAttribute("fail", "Invalid Session, Login First");
+			return "redirect:/login";
+		}
+	}
 }
